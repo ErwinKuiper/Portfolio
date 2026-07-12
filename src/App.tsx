b9721dd7833110758815d643db0c8c25042f9244
Erwin Kuiper <erwin.kuiper@hotmail.com>
2026-07-12 11:43:38 +0200

    feat: Add routed pages and responsive nav

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { Routes, Route } from 'react-router-dom'
 
 import './App.css'
 import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Projects from './pages/Projects/Projects';
+import Contact from './pages/Contact/Contact';
 
 function App() {
   return (
@@ -9,8 +13,10 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/about" element={<div>About</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,51 @@
 import { useState } from "react";
-import { Squash as Hamburger } from "hamburger-react";
+import { Twirl as Hamburger } from "hamburger-react";
+import { NavLink } from "react-router-dom";
 
 import styles from "./Navbar.module.css";
-import { NavLink } from "react-router-dom";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <>
-      <nav className={styles.mobileNav}>
+      <nav className={styles.navbar}>
         <h1>Erwin Kuiper</h1>
-        <Hamburger rounded toggled={isOpen} toggle={setIsOpen} />
+
+        {/* Desktop navigation */}
+        <div className={styles.desktopLinks}>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/projects">Projects</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </div>
+
+        {/* Mobile hamburger */}
+        <div className={styles.mobileMenu}>
+          <Hamburger
+            direction="left"
+            rounded
+            toggled={isOpen}
+            toggle={setIsOpen}
+          />
+        </div>
       </nav>
 
+      {/* Mobile menu */}
       <div className={`${styles.navLinks} ${isOpen ? styles.open : ""}`}>
-        <NavLink to="/" onClick={() => setIsOpen(false)}>
+        <NavLink to="/" onClick={closeMenu}>
           Home
         </NavLink>
-        <NavLink to="/about" onClick={() => setIsOpen(false)}>
+        <NavLink to="/projects" onClick={closeMenu}>
+          Projects
+        </NavLink>
+        <NavLink to="/about" onClick={closeMenu}>
           About
+        </NavLink>
+        <NavLink to="/contact" onClick={closeMenu}>
+          Contact
         </NavLink>
       </div>
     </>
